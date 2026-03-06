afc68ad72541727bc70aef82eaeb14757b8f1805
celine <wisohyun0@gmail.com>
2026-03-06 17:51:59 +0900

    test: 정상 작동하게끔 수정

--- components/Button/index.test.tsx
+++ components/Button/index.test.tsx
@@ -4,8 +4,4 @@ test("화면에 버튼이 나오는지 테스트", () => {
   render(<Button>버튼</Button>);
   const button = screen.getByRole("button", { name: "버튼" });
   expect(button).toBeInTheDocument();
-
-  // 실패하는 테스트 미리 작성
-  const h1 = screen.getByText("테스트");
-  expect(h1).toBeInTheDocument();
 });
